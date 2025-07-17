358d74e4f88de7527b79cb2ce3cbb5515cea7cc6
Burak123T <burak-9@live.dk>
2025-07-17 13:01:43 +0000

    added rad_logger

--- TRT_pipelines/TRT_Runtime_Inference_Case_D_5.py
+++ TRT_pipelines/TRT_Runtime_Inference_Case_D_5.py
@@ -8,6 +8,7 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 
 import time
+import ctypes
 
 class HostDeviceMem(object):
     def __init__(self, host_mem, device_mem):
@@ -17,11 +18,22 @@ class HostDeviceMem(object):
 
 print("\n" + "\033[96m" + "TensorRT version:" + "\033[0m \033[1m" + "{} \033[0m".format(tensorrt.__version__))
 
+librad = ctypes.CDLL('/usr/local/lib/librad_logger.so')
+print("\n---------------------------")
+print("| Loaded librad_logger.so |")
+print("---------------------------\n")
+
 TRT_LOGGER = tensorrt.Logger()
 RUNTIME = tensorrt.Runtime(TRT_LOGGER)
 BIN_OUTPUT_PATH = "../bin_outputs/eurosat_preprocessed_input_data.bin"
 context = None
 trt_engine = None
+
+rad_log_start = librad.log_start
+
+# int event_id, const char *event_name, const char *event_type, const char *event_data
+arg_types = [ctypes.c_int, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+rad_log_start.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
 
 CATEGORIES = [
     'AnnualCrop',
@@ -57,10 +69,15 @@ def preprocess_data(image_path):
         # Save as raw binary data (float32)
         norm_img.astype(np.float32).tofile(BIN_OUTPUT_PATH)
         print("\033[96m" + f"Preprocessed image saved to:" + "\033[0m \033[92m" + f"{BIN_OUTPUT_PATH}" + "\033[0m \n")
-
+        
+        preprocess_data__log_result = rad_log_start(20, b'preprocess_data FINISH', b'preprocessing data', str(norm_img).encode())
+        print("\033[94mrad_logger:\033[0m Data preprocessing finished (event {})".format(preprocess_data__log_result))
         return norm_img
 
     except Exception as e:
+        preprocess_data__log_result = rad_log_start(20, b'preprocess_data ERROR', b'preprocessing data', b'none')
+        print("\033[91mrad_logger:\033[0m Data preprocessing error (event {})".format(preprocess_data__log_result))
+        
         print("\033[91m" + f"Error during preprocessing: {e} \033[0m\n")
         sys.exit(1)
 
@@ -132,6 +149,10 @@ def allocate_buffers(engine):
             outputs.append(HostDeviceMem(host_mem, device_mem))
         else:
             inputs.append(HostDeviceMem(host_mem, device_mem))
+    
+    event_data = [inputs, outputs, bindings, stream]
+    allocate_buffers__log_result = rad_log_start(10, b'allocate_buffers', b'allocate buffers FINISH', str(event_data).encode())
+    print("\033[94mrad_logger:\033[0m TRT engine buffer allocation finished (event {})".format(allocate_buffers__log_result))
 
     return inputs, outputs, bindings, stream
 
@@ -151,13 +172,20 @@ def post_proc_results(pred_output):
 
         print("\033[96m" + "Predicted category:" + f"\033[92m {PREDICTED_CLASS_NAME}" + "\033[0m")
         print("\033[96m" + f"Probability:" + f"\033[92m {(predicted_probability * 100):.3f}%" + "\033[0m")
+        
+        post_proc_category__log_result = rad_log_start(30, b'post_proc_category', b'Predicted category FINISH', str(PREDICTED_CLASS_NAME).encode())
+        print("\033[94mrad_logger:\033[0m Image class prediction finished (event {})".format(post_proc_category__log_result))
 
-        # Optional: Print probabilities for all categories (for debugging or more detail)
-        #print("\nProbabilities for all categories:")
-        #for i, category_name in enumerate(CATEGORIES):
-            #probability = predictions[i]
-            #print(f"  {category_name}: {(probability * 100):.2f}%")
+        post_proc_probability__log_result = rad_log_start(31, b'post_proc_category', b'Predicted category FINISH', str(predicted_probability).encode())
+        print("\033[94mrad_logger:\033[0m Image prediction probability finished (event {})".format(post_proc_probability__log_result))
+
     except Exception as e:
+        post_proc_category__log_result = rad_log_start(30, b'post_proc_category', b'Predicted category ERROR', b'none')
+        print("\033[91mrad_logger:\033[0m Image class prediction finished (event {})".format(post_proc_category__log_result))
+
+        post_proc_probability__log_result = rad_log_start(31, b'post_proc_category', b'Predicted category ERROR', b'none')
+        print("\033[91mrad_logger:\033[0m Image prediction probability finished (event {})".format(post_proc_probability__log_result))
+
         print("\033[91m" + f"Error during post-processing results:\n {e}" + "\033[0m \n")
         exit()
 
@@ -204,7 +232,12 @@ if __name__ == '__main__':
             inf_output = run_inference(trt_engine, prep_image)
             post_proc_results(inf_output)
             end_time = time.time()
+
+            total_time = end_time - start_time
             print("\033[96m" + "Time measured:" + f"\033[92m {end_time - start_time}" + "\033[0m")
+
+            total_inf_time__log_result = rad_log_start(40, b'inference_time', b'Inference time calculation FINISH', str(total_time).encode())
+            print("\033[94mrad_logger:\033[0m Inference time finished (event {})".format(total_inf_time__log_result))
     except Exception as e:
         x = 0
         while True:
